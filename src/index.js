dbf1645b16dce95397f68cb9be9e6e38b90e262c
gaoshaoyun <276185876@qq.com>
2020-05-23 11:50:48 +0800

    保存

--- src/index.js
+++ src/index.js
@@ -4,61 +4,6 @@ import "./index.css";
 import App from "./App";
 import {Provider} from "mobx-react";
 import store from "./store";
-// import {observer} from "mobx-react";
-// import {observable, computed, action, autorun} from "mobx";
-
-// class todoStore {
-//   @observable todos = [
-//     {id: "0", title: "任务0", finished: false},
-//     {id: "1", title: "任务1", finished: false}
-//   ];
-//   @computed get unfinishedTodoCount() {
-//     return this.todos.filter(todo => !todo.finished).length;
-//   }
-
-//   /* 推导值 */
-//   get completedCount() {
-//     return this.todos.filter(todo => todo.completed).length;
-//   }
-// }
-
-// @observer
-// class TodoListView extends React.Component {
-//   render() {
-//     const {todos, unfinishedTodoCount} = this.props.todoList;
-//     return (
-//       <div>
-//         <ul>
-//           {todos.map(todo => (
-//             <TodoView todo={todo} key={todo.id} />
-//           ))}
-//         </ul>
-//         Tasks left: {unfinishedTodoCount}
-//       </div>
-//     );
-//   }
-// }
-
-// const TodoView = ({todo}) => (
-//   <li>
-//     <input
-//       type="checkbox"
-//       checked={todo.finished}
-//       onChange={() => (todo.finished = !todo.finished)}
-//     />
-//     {todo.title}
-//   </li>
-// );
-// const store = new todoStore();
-
-// autorun(function() {
-//   console.log("Completed %d of %d items", store.todos.length);
-// });
-
-// ReactDOM.render(
-//   <TodoListView todoList={store} />,
-//   document.getElementById("root")
-// );
 
 ReactDOM.render(
   <Provider {...store}>
--- src/service/login.js
+++ src/service/login.js
@@ -10,18 +10,6 @@ const LoginService = {
         }
       }, 1000);
     });
-  },
-  // 获取更多信息
-  getMoreUserInfo(userInfo) {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        if (userInfo.id === 123) {
-          resolve({...userInfo, score: "100"});
-        } else {
-          reject({msg: "获取详细信息错误"});
-        }
-      }, 1000);
-    });
   }
 };
 export default LoginService;
