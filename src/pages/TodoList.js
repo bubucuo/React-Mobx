4bac5b1acb4efb3be2063fdbf1c3134321943b9d
gaoshaoyun <276185876@qq.com>
2020-09-10 18:31:09 +0800

    save ls3 给组件添加响应式

--- src/pages/TodoList.js
+++ src/pages/TodoList.js
@@ -1,5 +1,6 @@
 import React, {Component} from "react";
 import {observer} from "mobx-react";
+import {Observer, useObserver} from "mobx-react-lite";
 
 @observer
 class TodoList extends Component {
@@ -23,8 +24,45 @@ class TodoList extends Component {
 }
 export default TodoList;
 
-const Todo = observer(({todo, change}) => {
-  return (
+// 给组件添加响应式
+
+// 方法1： hoc observer（高阶组件： 是个函数，这个函数接收组件作为参数，并且返回一个新组件）
+// const Todo = observer(({todo, change}) => {
+//   return (
+//     <li>
+//       <input
+//         type="checkbox"
+//         checked={todo.finished}
+//         //onChange={() => (todo.finished = !todo.finished)}
+//         onChange={() => change(todo)}
+//       />
+//       {todo.title}
+//     </li>
+//   );
+// });
+
+// 方法2：Observer component
+// const Todo = ({todo, change}) => {
+//   return (
+//     <Observer>
+//       {() => (
+//         <li>
+//           <input
+//             type="checkbox"
+//             checked={todo.finished}
+//             //onChange={() => (todo.finished = !todo.finished)}
+//             onChange={() => change(todo)}
+//           />
+//           {todo.title}
+//         </li>
+//       )}
+//     </Observer>
+//   );
+// };
+
+// 方法3：useObserver hook方法
+const Todo = ({todo, change}) => {
+  return useObserver(() => (
     <li>
       <input
         type="checkbox"
@@ -34,5 +72,5 @@ const Todo = observer(({todo, change}) => {
       />
       {todo.title}
     </li>
-  );
-});
+  ));
+};
