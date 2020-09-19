007248005c412d4d9e4f1c07503605f8c7ec6642
gaoshaoyun <276185876@qq.com>
2020-09-19 13:14:53 +0800

    init ls 5

--- src/pages/TodoList.js
+++ src/pages/TodoList.js
@@ -1,55 +1,29 @@
 import React, {Component, Children} from "react";
 import {observer, Observer, useObserver} from "mobx-react";
 
-@observer
+// import {useObserver} from "../k-mobx-react-lite/index";
+
+// @observer
 class TodoList extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      input: "",
-      count: 0
-    };
-  }
   render() {
-    const {input, count} = this.state;
     return (
       <div>
         <h3>TodoList</h3>
-
-        <input
-          type="text"
-          value={input}
-          onChange={e => this.setState({input: e.target.value})}
-        />
-
-        <button onClick={() => this.setState({count: count + 1})}>
-          add click {count}
-        </button>
 
         {this.props.todoStore.todos.map(todo => (
           <Todo
             key={todo.id}
             todo={todo}
             change={this.props.todoStore.change}
-            count={{count}}
           />
         ))}
         <p>未完成任务： {this.props.todoStore.unfinishedCount}个</p>
-        <Child count={{count}} />
       </div>
     );
   }
 }
 
 export default TodoList;
-
-const Child = React.memo(
-  props => {
-    console.log("child props"); //sy-log
-    return <div>Child</div>;
-  },
-  (prevProps, nextProps) => prevProps.count.count === nextProps.count.count
-);
 
 // 如何给组件添加响应式
 // // 方法1： observer hoc（高阶组件：接收组件为参数并且返回一个新组件的函数）
